3ef2c05fcdd40e1ef570f5ad4595bfa93da8a589
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-07-16 21:39:25 +0530

    Add files via upload

---
+++ Day 021/657. Robot Return to Origin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0;
+        int y=0;
+        for(int i=0; i<moves.size(); i++){
+            if(moves[i] == 'U'){
+                y++;
+            }
+            else if(moves[i] == 'D'){
+                y--;
+            }
+            else if(moves[i] == 'L'){
+                x--;
+            }
+            else if(moves[i] == 'R'){
+                x++;
+            }
+        }
+        if(x==0 && y==0){
+            return true;
+        }
+        return false;
+    }
+};
