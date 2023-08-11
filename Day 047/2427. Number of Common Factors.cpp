7ec82b4e3ff27f9237a0522a11aaf65625c5b5a1
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-08-11 22:49:54 +0530

    Add files via upload

---
+++ Day 047/2427. Number of Common Factors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int r;
+        while(b!=0){
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        int c=0;
+        for (int i = 1;i<=a;i++){
+            if (a%i ==0){
+                c++;
+            }
+        }
+        return c;
+    }
+};
