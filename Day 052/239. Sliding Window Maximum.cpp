d79e5205eb087be7fedca65d019f7d6eb7204e85
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-08-16 22:41:07 +0530

    Add files via upload

---
+++ Day 052/239. Sliding Window Maximum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+
+        list<int>l;
+        vector<int>ans;
+        
+        while(j<nums.size()){
+            while(!l.empty() && l.back()<nums[j]){
+                l.pop_back();
+            }
+            l.push_back(nums[j]);
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+              ans.push_back(l.front());
+
+              if(l.front()==nums[i]){
+                  l.pop_front();
+              }
+              i++;
+              j++;
+            }
+        }
+        return ans;
+    }
+};
