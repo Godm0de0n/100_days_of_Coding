2ec1ff7e5eeff6c940def03fc2ba2ade49bfe9a5
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-08-17 23:08:24 +0530

    Add files via upload

---
+++ Day 053/509. Fibonacci Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0;
+        int b = 1;
+        int s;
+        
+        if (n == 0) {
+            return a;
+        } else if (n == 1) {
+            return b;
+        } else {
+            for (int i = 2; i <= n; i++) {
+                s = a + b;
+                a = b;
+                b = s;
+            }
+            return s;
+        }
+    }
+};
