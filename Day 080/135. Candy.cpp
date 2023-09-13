b12195023d3bcf92d8c0eabc296375d30dd9e014
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-09-13 23:58:49 +0530

    Add files via upload

---
+++ Day 080/135. Candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> a(n, 1);
+
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                a[i] = a[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                a[i] = max(a[i], a[i + 1] + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int c : a) {
+            ans += c;
+        }
+
+        return ans;
+    }
+};
