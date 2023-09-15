5b574f80acab1ab867999fc7fa1033e42cd08c43
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-09-15 23:55:28 +0530

    Add files via upload

---
+++ Day 082/2806. Account Balance After Rounded Purchase.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int accountBalanceAfterPurchase(int purchaseAmount) {
+        int a = purchaseAmount %10;
+        if(a>=0 && a<5){
+            purchaseAmount -= a;
+        } 
+        else {
+            purchaseAmount += 10-a;
+        }
+        return 100 - purchaseAmount;
+
+    }
+};
