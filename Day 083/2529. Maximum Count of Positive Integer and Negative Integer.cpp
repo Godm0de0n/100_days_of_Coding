278a44e402685fc03124ad11681acf34c1684f64
Abhisek Nag <110175017+Godm0de0n@users.noreply.github.com>
2023-09-16 15:56:22 +0530

    Add files via upload

---
+++ Day 083/2529. Maximum Count of Positive Integer and Negative Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int p=0;
+        int n=0;
+        for (int i =0;i<nums.size();i++){
+            if (nums[i]>0){
+                p++;
+            }
+            else if(nums[i]<0){
+                n++;
+            }
+        }
+        return max(p,n);
+    }
+};
